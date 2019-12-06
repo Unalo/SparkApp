35fe9975e2c8e81495a1f50b10e9dcf89beba066
unalo <unalomtshokotsha@gmail.com>
2019-12-06 19:07:11 +0200

    added more route

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,9 @@
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
@@ -18,26 +21,53 @@ public class HelloWorld {
             staticFiles.location("/public");
 
             port(getHerokuAssignedPort());
+             Map<String, Integer> greetedUsers = new HashMap<>();
+            Map<String, Object> dataMap = new HashMap<>();
 
-            get("/", (req, res) -> {
-                return new ModelAndView(new HashMap<>(), "index.hbs");
-            }, new HandlebarsTemplateEngine());
+
 
             post("/hello", (req, res) -> {
                 // get form data values
-                String name = req.queryParams("firstName");
-                Map<String, String> dataMap = new HashMap<>();
-                Map<String, String> errorMap = new HashMap<>();
-                //System.out.println("nhgaphakathi");
-                //dataMap.put("name", "Please enter Name!!");
+                String name = req.queryParams("firstName").substring(0,1).toUpperCase() + req.queryParams("firstName").substring(1).toLowerCase();
+                String lang = req.queryParams("language");
+                Map<String, Integer> counterMap = new HashMap<>();
+
+//                if (greetedUsers.containsKey(name)) {
+//                    greetedUsers.put(name, greetedUsers.get(name) + 1);
+//                }
+
+                if (lang.equals("English")) {
+                    dataMap.put("message", "Hello ");
+                    greetedUsers.put(name, 1);
+                } else if (lang.equals("Isixhosa")) {
+                    dataMap.put("message", "Molo ");
+                    greetedUsers.put(name, 1);
+                } else if (lang.equals("Afrikaans")) {
+                    dataMap.put("message", "Goer MOre " );
+                    greetedUsers.put(name, 1);
+                }
+
                 dataMap.put("name", name);
-
-
 
                 // put the values from the form for Handlebars to use
 
                 return new ModelAndView(dataMap, "hello.hbs");
 
+            }, new HandlebarsTemplateEngine());
+
+            get("/", (req, res) -> {
+                int counter = greetedUsers.size();
+                dataMap.put("greeted", counter);
+                return new ModelAndView(dataMap, "index.hbs");
+            }, new HandlebarsTemplateEngine());
+
+            get("/users", (req, res) -> {
+                List<String> greet = new ArrayList();
+                for (String name : greetedUsers.keySet()) {
+                    greet.add(name);
+                }
+                dataMap.put("users", greet);
+                return new ModelAndView(dataMap, "greeted.hbs");
             }, new HandlebarsTemplateEngine());
         }
     }
